0c8127e3e7b06351fa251055a5eb461a53fbd824
Aurélien <aurelien.strimelle@bstorm.be>
2024-02-21 13:30:04 +0100

    Initialize Projet 🦉

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config({ override: true });
+
+// Imports
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const session = require('express-session');
+const homeRouter = require('./routes/home.router');
+
+// Récuperation des variables d'env
+const { NODE_ENV, PORT, SESSION_SECRET } = process.env;
+
+// Initilisation du Web Server
+const app = express();
+
+// - Config du moteur de vue
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
+
+// - Middlewares
+//*  Logger
+app.use(morgan('tiny'));
+//*  Fichier public
+app.use(express.static('./public'));
+//*  Données des formulaires
+app.use(express.urlencoded({ extended: true }));
+//*  Session
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET
+}));
+
+// - Routing
+app.use(homeRouter);
+
+// Démarrage du Web serveur
+app.listen(PORT, () => {
+    console.log(`Web server is running on ${PORT} (${NODE_ENV})`);
+});
---
+++ controllers/home.controller.js
@@ -0,0 +1,9 @@
+const homeController = {
+
+    index: async (req, res) => {
+        res.render('home/index');
+    }
+
+};
+
+module.exports = homeController;
