4a29f3ba379f12f3d3cca5c48533d0b8f54bdf89
Aurélien <aurelien.strimelle@bstorm.be>
2024-02-27 09:15:26 +0100

    Remove inject data in home page (Dev test) 🐱‍💻

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -4,19 +4,6 @@ const homeController = {
 
     index: async (req, res) => {
 
-        // FIXME Demo de l'utilisation. A supprimer !
-        const article = {
-            title : 'Demo MongoDB + Mongoose',
-            slug: 'demo-mongodb-mongoose',
-            tag: ['Express', 'MongoDB'],
-            desc: 'Ceci est la fin de la démo 😲',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam impedit hic molestias rem a architecto animi eligendi corrupti, dolorem in iure autem quisquam iste doloremque fugiat odio expedita ab commodi.'
-        }
-        const data = await articleService.create(article)
-        console.log(data);
-
-        await articleService.addComment(data.slug, 'Fonctioone ? 😲')
-
         res.render('home/index');
     }
 
